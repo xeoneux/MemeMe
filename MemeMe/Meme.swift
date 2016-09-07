d2fa5b5f154f055b68925deb47c1492f1d31a427
Aayush Kapoor <xeoneux@icloud.com>
2016-09-08 02:07:45 +0530

    feat: add meme struct

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Aayush Kapoor on 08/09/16.
+//  Copyright © 2016 Aayush Kapoor. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let memedImage: UIImage
+    let originalImage: UIImage
+}
