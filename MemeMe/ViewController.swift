e1889f01ac9d7c221b89d88b2d1a12354ae5bf77
Aayush Kapoor <xeoneux@icloud.com>
2016-09-07 11:19:13 +0530

    feat: set picked image

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
 
     @IBAction func pickAnImage(sender: AnyObject) {
         let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
         presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
