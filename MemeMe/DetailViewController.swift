6cefa4ac7607743db5ead5f4d7cc03b891571310
Aayush Kapoor <xeoneux@icloud.com>
2016-09-11 10:53:57 +1000

    fix: detail view controller

--- MemeMe/DetailViewController.swift
+++ MemeMe/DetailViewController.swift
@@ -13,20 +13,9 @@ class DetailViewController: UIViewController {
     var meme: Meme!
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var shareButton: UIBarButtonItem!
-    @IBOutlet weak var cancelButton: UIBarButtonItem!
 
     override func viewWillAppear(animated: Bool) {
         imageView.image = meme.memedImage
-    }
-
-    @IBAction func shareAction(sender: UIBarButtonItem) {
-        let activityViewController = UIActivityViewController(activityItems: [meme.memedImage], applicationActivities: nil)
-        presentViewController(activityViewController, animated: true, completion: nil)
-    }
-
-    @IBAction func cancelAction(sender: UIBarButtonItem) {
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
